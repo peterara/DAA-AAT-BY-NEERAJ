2b51e2dd9971b6c511da01b100e009e3004611cc
peterara <neerajkumarpers@gmail.com>
2024-07-14 22:15:20 +0530

    Add files via upload

---
+++ quicksort 1.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def quickSort(arr):
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
